e0f38370b810672b7dfa68074389f3f278c23840
roshankhaire <roshankhaire1992@gmail.com>
2022-08-11 20:11:57 -0700

    first commit

---
+++ query.js
@@ -0,0 +1,11 @@
+
+
+var ite=document.getElementsByTagName('li');
+console.log(ite);
+console.log(ite[4].style.color='red');
+
+
+
+var ite=document.getElementsByClassName('li');
+console.log(ite);
+console.log(ite[4].style.color='red');
